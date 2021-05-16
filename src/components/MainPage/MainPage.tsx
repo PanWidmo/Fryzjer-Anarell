bf62cfa0ab71bb5a3197a1e0885422b1aaba5927
PanWidmo <tomasz.baranww@gmail.com>
2021-05-16 13:33:37 +0200

    Installed @types/styled-components, styled-components, styled-reset

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,23 @@
+import React,{FC} from 'react';
+import styled from 'styled-components';
+
+
+const Wrapper = styled.section`
+
+`;
+
+const Content = styled.div`
+
+`;
+
+const MainPage: FC = () => {
+    return (
+        <Wrapper>
+            <Content>
+                Strona fryzjer pozdrawiam :)
+            </Content>
+        </Wrapper>
+    );
+};
+
+export default MainPage;
